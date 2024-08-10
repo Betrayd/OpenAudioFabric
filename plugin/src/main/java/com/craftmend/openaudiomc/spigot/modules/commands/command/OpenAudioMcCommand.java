8bf08778841e0d54ef67f1dcb2b21f00154f2cdd
Da0neDatGotAway <43645756+Da0neDatGotAway@users.noreply.github.com>
2024-08-10 16:02:22 -0700

    moved into the spigot follder instead of bungee cause I did not know where we were deving

---
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/commands/command/OpenAudioMcCommand.java
@@ -0,0 +1,68 @@
+package com.craftmend.openaudiomc.spigot.modules.commands.command;
+
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
+
+import com.craftmend.openaudiomc.OpenAudioMc;
+import com.craftmend.openaudiomc.generic.environment.MagicValue;
+import com.craftmend.openaudiomc.spigot.modules.playlists.PlaylistService;
+import com.craftmend.openaudiomc.spigot.modules.playlists.models.Playlist;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import com.openaudiofabric.OpenAudioFabric;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.server.command.CommandManager.RegistrationEnvironment;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.server.command.ServerCommandSource;
+
+public class OpenAudioMcCommand {
+
+        public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess,
+            RegistrationEnvironment environment) {
+        // .requires(s -> s.hasPermissionLevel(2))
+        final LiteralCommandNode<ServerCommandSource> oaCommandNode = dispatcher.register(literal("openaudiomc")
+                .then(literal("playlist")
+                    .then(literal("create")
+                        .then(argument("playlistName", StringArgumentType.string()).suggests(playlistsSuggestor())
+                            .executes(OpenAudioMcCommand::sendVersion)))
+                    .then(literal("delete"))
+                    .then(literal("list"))
+                    .then(literal("view"))
+                    .then(literal("add")))
+                .executes(OpenAudioMcCommand::sendVersion));
+        dispatcher.register(literal("oa").redirect(oaCommandNode));
+        dispatcher.register(literal("oam").redirect(oaCommandNode));
+        dispatcher.register(literal("openaudio").redirect(oaCommandNode));
+    }
+
+    public static SuggestionProvider<ServerCommandSource> playlistsSuggestor() {
+        return (ctx, builder) -> {
+
+            PlaylistService playlistService = OpenAudioMc.getService(PlaylistService.class);
+            for (Playlist s : playlistService.getAll()) {
+                builder.suggest(s.getName());
+            }
+
+            return builder.buildFuture();
+        };
+    }
+
+    private static int sendVersion(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        context.getSource().sendFeedback(() -> {return Text.literal(MagicValue.COMMAND_PREFIX.get(String.class) + "OpenAudioFabric version " + FabricLoader.getInstance().getModContainer(OpenAudioFabric.modID).get().getMetadata().getVersion() + ". For help, please use /openaudio help");}, false);
+        return 1;
+    }
+
+    public static int playlistCreate(CommandContext<ServerCommandSource> context) throws CommandSyntaxException 
+    {
+        return 1;
+    }
+}
--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/commands/command/VolumeCommand.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/commands/command/VolumeCommand.java
@@ -1,73 +1,60 @@
 package com.craftmend.openaudiomc.spigot.modules.commands.command;
 
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
 
 import com.craftmend.openaudiomc.OpenAudioMc;
-import com.craftmend.openaudiomc.generic.commands.helpers.CommandMiddewareExecutor;
-import com.craftmend.openaudiomc.generic.commands.interfaces.CommandMiddleware;
-import com.craftmend.openaudiomc.generic.commands.middleware.CatchCrashMiddleware;
-import com.craftmend.openaudiomc.generic.commands.middleware.CatchLegalBindingMiddleware;
-import com.craftmend.openaudiomc.generic.commands.middleware.CleanStateCheckMiddleware;
+import com.craftmend.openaudiomc.generic.client.objects.ClientConnection;
+import com.craftmend.openaudiomc.generic.networking.interfaces.NetworkingService;
 import com.craftmend.openaudiomc.generic.platform.Platform;
-import com.craftmend.openaudiomc.generic.proxy.interfaces.UserHooks;
 import com.craftmend.openaudiomc.generic.storage.enums.StorageKey;
-import com.craftmend.openaudiomc.generic.user.User;
-import com.craftmend.openaudiomc.spigot.modules.players.SpigotPlayerService;
-import com.craftmend.openaudiomc.spigot.modules.players.objects.SpigotConnection;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.tree.LiteralCommandNode;
 
-import lombok.NoArgsConstructor;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.server.command.CommandManager.RegistrationEnvironment;
+import net.minecraft.text.Text;
+import net.minecraft.server.command.ServerCommandSource;
 
-@NoArgsConstructor
-public class VolumeCommand implements CommandExecutor {
-
-    private final CommandMiddleware[] commandMiddleware = new CommandMiddleware[] {
-            new CatchLegalBindingMiddleware(),
-            new CatchCrashMiddleware(),
-            new CleanStateCheckMiddleware()
-    };
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-        User sua = OpenAudioMc.resolveDependency(UserHooks.class).fromCommandSender(sender);
-        if (CommandMiddewareExecutor.shouldBeCanceled(sua, null, commandMiddleware)) return true;
-
-        if (sender instanceof Player) {
-            SpigotConnection spigotConnection = OpenAudioMc.getService(SpigotPlayerService.class).getClient(((Player) sender).getUniqueId());
-
-            if (!spigotConnection.getClientConnection().isConnected()) {
-                String message = Platform.translateColors(StorageKey.MESSAGE_CLIENT_NOT_CONNECTED.getString());
-                sender.sendMessage(message);
-                return true;
-            }
-
-            if (args.length == 0) {
-                sender.sendMessage(Platform.translateColors(
-                        StorageKey.MESSAGE_CLIENT_VOLUME_INVALID.getString())
-                );
-                return true;
-            }
-
-            try {
-                int volume = Integer.parseInt(args[0]);
-                //check if in range
-                if (volume < 0 || volume > 100) {
-                    String message = Platform.translateColors(StorageKey.MESSAGE_CLIENT_VOLUME_INVALID.getString());
-                    sender.sendMessage(message);
-                    return true;
-                } else {
-                    spigotConnection.getClientConnection().setVolume(volume);
-                }
-            } catch (Exception e) {
-                String message = Platform.translateColors(StorageKey.MESSAGE_CLIENT_VOLUME_INVALID.getString());
-                sender.sendMessage(message);
-                return true;
-            }
-        } else {
-            sender.sendMessage("This command can only be used by players");
-        }
-        return true;
+public class VolumeCommand {
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess,
+            RegistrationEnvironment environment) {
+        // .requires(s -> s.hasPermissionLevel(2))
+        final LiteralCommandNode<ServerCommandSource> volumeCommandNode = dispatcher.register(literal("volume")
+                .then(argument("percent", IntegerArgumentType.integer(0, 100)).executes(VolumeCommand::execute)));
+        dispatcher.register(literal("setvolume").redirect(volumeCommandNode));
+        dispatcher.register(literal("vol").redirect(volumeCommandNode));
     }
 
+    private static int execute(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        if (context.getSource().getPlayer() == null) {
+            context.getSource().sendFeedback(() -> {return Text.literal("This command can only be used by players");}, false);
+            return 0;
+        }
+
+        ClientConnection clientConnection = OpenAudioMc.getService(NetworkingService.class)
+                .getClient(context.getSource().getPlayer().getUuid());
+
+        if (!clientConnection.isConnected()) {
+            context.getSource().sendFeedback(() -> {return Text.literal(Platform.translateColors(StorageKey.MESSAGE_CLIENT_VOLUME_INVALID.getString()));}, false);
+            return 0;
+        }
+
+        try {
+            int volume = IntegerArgumentType.getInteger(context, "percent");
+            // check if in range, just in case I don't know what I'm doing
+            if (volume >= 0 || volume <= 100) {
+                clientConnection.setVolume(volume);
+                return 1;
+            }
+        } catch (Exception e) {
+            String message = Platform.translateColors(
+                    OpenAudioMc.getInstance().getConfiguration().getString(StorageKey.MESSAGE_CLIENT_VOLUME_INVALID));
+            context.getSource().sendFeedback(() -> {return Text.literal(message);}, false);
+        }
+        return 0;
+    }
 }
