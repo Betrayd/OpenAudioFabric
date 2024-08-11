7b16143aea5d17dd2ba09079dbbaf5f15a0db364
Da0neDatGotAway <43645756+Da0neDatGotAway@users.noreply.github.com>
2024-08-11 09:40:48 -0700

    added speaker give command and speaker gc command to OpenAudioMc command

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/commands/command/OpenAudioMcCommand.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/commands/command/OpenAudioMcCommand.java
@@ -7,11 +7,17 @@ import java.util.Collection;
 import java.util.Locale;
 
 import com.craftmend.openaudiomc.OpenAudioMc;
+import com.craftmend.openaudiomc.generic.commands.objects.CommandError;
 import com.craftmend.openaudiomc.generic.environment.MagicValue;
+import com.craftmend.openaudiomc.generic.media.MediaService;
+import com.craftmend.openaudiomc.generic.media.utils.Validation;
 import com.craftmend.openaudiomc.generic.platform.OaColor;
+import com.craftmend.openaudiomc.generic.storage.enums.StorageKey;
 import com.craftmend.openaudiomc.spigot.modules.playlists.PlaylistService;
 import com.craftmend.openaudiomc.spigot.modules.playlists.models.Playlist;
 import com.craftmend.openaudiomc.spigot.modules.playlists.models.PlaylistEntry;
+import com.craftmend.openaudiomc.spigot.modules.speakers.tasks.SpeakerGarbageCollection;
+import com.craftmend.openaudiomc.spigot.modules.speakers.utils.SpeakerUtils;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
@@ -24,6 +30,7 @@ import com.openaudiofabric.OpenAudioFabric;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.server.command.CommandManager.RegistrationEnvironment;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.ClickEvent;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
@@ -57,10 +64,22 @@ public class OpenAudioMcCommand {
                                                 .executes(OpenAudioMcCommand::playlistView)))
                                 .then(literal("add")
                                         .then(argument("playlist_name", StringArgumentType.string())
-                                        .suggests(playlistsSuggestor())
+                                                .suggests(playlistsSuggestor())
                                                 .then(argument("source_url", StringArgumentType.string())
-                                                        .executes(OpenAudioMcCommand::playlistAdd))))
-                                .executes(OpenAudioMcCommand::sendVersion)));
+                                                        .executes(OpenAudioMcCommand::playlistAdd)))))
+                        .then(literal("speaker")
+                                .then(argument("source_url", StringArgumentType.string())
+                                        .then(argument("radius", IntegerArgumentType.integer())
+                                                .executes(ctx -> speakerGive(ctx.getSource(),
+                                                        StringArgumentType.getString(ctx, "source_url"),
+                                                        IntegerArgumentType.getInteger(ctx, "radius"))))
+                                        .executes(ctx -> speakerGive(ctx.getSource(),
+                                                StringArgumentType.getString(ctx, "source_url"), 10)))
+                                .then(literal("gc")
+                                        .then(argument("confirm", StringArgumentType.string())
+                                                .executes(OpenAudioMcCommand::speakerGarbageCollection))
+                                        .executes(OpenAudioMcCommand::speakerGarbageCollectionWarn)))
+                        .executes(OpenAudioMcCommand::sendVersion));
         dispatcher.register(literal("oa").redirect(oaCommandNode));
         dispatcher.register(literal("oam").redirect(oaCommandNode));
         dispatcher.register(literal("openaudio").redirect(oaCommandNode));
@@ -90,9 +109,9 @@ public class OpenAudioMcCommand {
     private static PlaylistService playlistService = (PlaylistService.class)
             .cast(OpenAudioMc.getInstance().getServiceManager().loadService(PlaylistService.class));
 
-    private static PlaylistService getPlaylistService()
-    {
-        return (PlaylistService.class).cast(OpenAudioMc.getInstance().getServiceManager().loadService(PlaylistService.class));
+    private static PlaylistService getPlaylistService() {
+        return (PlaylistService.class)
+                .cast(OpenAudioMc.getInstance().getServiceManager().loadService(PlaylistService.class));
     }
 
     public static int playlistCreate(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
@@ -147,7 +166,8 @@ public class OpenAudioMcCommand {
     }
 
     private static int playlistRemove(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        Playlist playlist = playlistService.getPlaylist(StringArgumentType.getString(context, "playlist_name").toLowerCase(Locale.ROOT));
+        Playlist playlist = playlistService
+                .getPlaylist(StringArgumentType.getString(context, "playlist_name").toLowerCase(Locale.ROOT));
         Integer index = IntegerArgumentType.getInteger(context, "track_index");
 
         if (playlist == null) {
@@ -161,12 +181,15 @@ public class OpenAudioMcCommand {
             return 0;
         }
         getPlaylistService().saveAll();
-        context.getSource().sendFeedback(() -> {return Text.literal("Removed track at index " + index + " from playlist " + playlist.getName());}, false);
+        context.getSource().sendFeedback(() -> {
+            return Text.literal("Removed track at index " + index + " from playlist " + playlist.getName());
+        }, false);
         return 1;
     }
 
     private static int playlistView(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        Playlist playlist = playlistService.getPlaylist(StringArgumentType.getString(context, "playlist_name").toLowerCase(Locale.ROOT));
+        Playlist playlist = playlistService
+                .getPlaylist(StringArgumentType.getString(context, "playlist_name").toLowerCase(Locale.ROOT));
 
         if (playlist == null) {
             context.getSource().sendError(Text.literal("A playlist with that name does not exist"));
@@ -174,21 +197,34 @@ public class OpenAudioMcCommand {
         }
 
         if (playlist.getEntries().isEmpty()) {
-            context.getSource().sendError(Text.literal("Playlist " + playlist.getName() + " by " + playlist.getCreatedBy() + " is empty"));
+            context.getSource().sendError(
+                    Text.literal("Playlist " + playlist.getName() + " by " + playlist.getCreatedBy() + " is empty"));
             return 0;
         }
 
-        context.getSource().sendFeedback(() -> {return Text.literal("Playlist " + playlist.getName() + " by " + playlist.getCreatedBy() + " has " + playlist.getEntries().size() + " tracks");}, false);
+        context.getSource().sendFeedback(() -> {
+            return Text.literal("Playlist " + playlist.getName() + " by " + playlist.getCreatedBy() + " has "
+                    + playlist.getEntries().size() + " tracks");
+        }, false);
         for (PlaylistEntry orderedEntry : playlist.getOrderedEntries()) {
-            context.getSource().sendFeedback(() -> {return Text.literal(OaColor.RED + " - " + OaColor.DARK_RED + orderedEntry.getIndex() + OaColor.AQUA + " " + orderedEntry.getMedia()).setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "openaudio playlist remove " + playlist.getName() + " " + orderedEntry.getIndex())));}, false);
+            context.getSource().sendFeedback(() -> {
+                return Text
+                        .literal(OaColor.RED + " - " + OaColor.DARK_RED + orderedEntry.getIndex() + OaColor.AQUA + " "
+                                + orderedEntry.getMedia())
+                        .setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND,
+                                "openaudio playlist remove " + playlist.getName() + " " + orderedEntry.getIndex())));
+            }, false);
         }
 
-        context.getSource().sendFeedback(() -> {return Text.literal("You can click on a track to remove it from the playlist");}, false); 
+        context.getSource().sendFeedback(() -> {
+            return Text.literal("You can click on a track to remove it from the playlist");
+        }, false);
         return 1;
     }
 
     private static int playlistAdd(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        Playlist playlist = playlistService.getPlaylist(StringArgumentType.getString(context, "playlist_name").toLowerCase(Locale.ROOT));
+        Playlist playlist = playlistService
+                .getPlaylist(StringArgumentType.getString(context, "playlist_name").toLowerCase(Locale.ROOT));
         String source = StringArgumentType.getString(context, "source_url");
 
         if (playlist == null) {
@@ -199,7 +235,65 @@ public class OpenAudioMcCommand {
         playlist.addEntry(new PlaylistEntry(source));
 
         getPlaylistService().saveAll();
-        context.getSource().sendFeedback(() -> {return Text.literal("Added " + source + " to playlist " + playlist.getName());}, false);
+        context.getSource().sendFeedback(() -> {
+            return Text.literal("Added " + source + " to playlist " + playlist.getName());
+        }, false);
+        return 1;
+    }
+
+    private static int speakerGive(ServerCommandSource sender, String source, int radius) {
+
+        ServerPlayerEntity player = sender.getPlayer();
+        if (player != null) {
+            sender.sendError(Text.literal("Only players can receive a speaker item."));
+            return 0;
+        }
+
+        if (radius > StorageKey.SETTINGS_SPEAKER_MAX_RANGE.getInt()) {
+            sender.sendError(Text.literal("The radius is too large. The maximum radius is "
+                    + StorageKey.SETTINGS_SPEAKER_MAX_RANGE.getInt()));
+            return 0;
+        }
+
+        if (Validation.isStringInvalid(source)) {
+            sender.sendError(Text.literal("Invalid source url."));
+            return 0;
+        }
+
+        player.getInventory()
+                .insertStack(SpeakerUtils.getSkull(OpenAudioMc.getService(MediaService.class).process(source), radius));
+        sender.sendFeedback(() -> {
+            return Text.literal(
+                    "Speaker media created! You've received a Speaker skull in your inventory. Placing it anywhere in the world will add the configured sound in the are.");
+        }, false);
+        return 1;
+    }
+
+    private static int speakerGarbageCollectionWarn(CommandContext<ServerCommandSource> context)
+            throws CommandSyntaxException {
+        context.getSource().sendFeedback(() -> {
+            return Text.literal(OaColor.RED
+                    + "WARNING! THIS COMMAND WILL LOAD AN INSANE AMOUNT OF CHUNKS IF YOU USE SPEAKERS THROUGHOUT YOUR SERVER. RUN "
+                    + OaColor.YELLOW + "/oa speaker gc confirm" + OaColor.RED
+                    + " IF YOU ARE 100% SURE THAT YOU WANT TO CONTINUE");
+        }, false);
+        return 1;
+    }
+
+    private static int speakerGarbageCollection(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        if (StringArgumentType.getString(context, "confirm").equalsIgnoreCase("confirm")) {
+            return speakerGarbageCollectionWarn(context);
+        }
+
+        context.getSource().sendFeedback(() -> {
+            return Text.literal("Starting garbage collector...");
+        }, false);
+        SpeakerGarbageCollection sgc = new SpeakerGarbageCollection();
+        // run the wrapper twice to force a cache refresh at the end
+        sgc.run();
+        context.getSource().sendFeedback(() -> {
+            return Text.literal("Full garbage collection sweep finished");
+        }, false);
         return 1;
     }
 }
