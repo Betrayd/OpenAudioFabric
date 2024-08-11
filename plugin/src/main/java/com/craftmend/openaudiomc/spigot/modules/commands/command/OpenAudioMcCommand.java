35156b5bcfcacc1e5d9234a003a1d024cd4b4a0d
Da0neDatGotAway <43645756+Da0neDatGotAway@users.noreply.github.com>
2024-08-10 18:14:15 -0700

    working on adding all the OA playlisst commands

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/commands/command/OpenAudioMcCommand.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/commands/command/OpenAudioMcCommand.java
@@ -3,14 +3,18 @@ package com.craftmend.openaudiomc.spigot.modules.commands.command;
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
+import java.util.Collection;
 import java.util.Locale;
 
 import com.craftmend.openaudiomc.OpenAudioMc;
 import com.craftmend.openaudiomc.generic.commands.objects.CommandError;
 import com.craftmend.openaudiomc.generic.environment.MagicValue;
+import com.craftmend.openaudiomc.generic.platform.OaColor;
 import com.craftmend.openaudiomc.spigot.modules.playlists.PlaylistService;
 import com.craftmend.openaudiomc.spigot.modules.playlists.models.Playlist;
+import com.craftmend.openaudiomc.spigot.modules.playlists.models.PlaylistEntry;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
@@ -29,19 +33,29 @@ public class OpenAudioMcCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess,
             RegistrationEnvironment environment) {
         // .requires(s -> s.hasPermissionLevel(2))
-        final LiteralCommandNode<ServerCommandSource> oaCommandNode = dispatcher.register(literal("openaudiomc")
-                .then(literal("playlist")
-                        .then(literal("create")
-                                .then(argument("playlistName", StringArgumentType.string())
-                                        .executes(OpenAudioMcCommand::playlistCreate)))
-                        .then(literal("delete")
-                                .then(argument("playlistName", StringArgumentType.string())
-                                        .suggests(playlistsSuggestor())
-                                        .executes(OpenAudioMcCommand::playlistDelete)))
-                        .then(literal("list"))
-                        .then(literal("view"))
-                        .then(literal("add"))
-                        .executes(OpenAudioMcCommand::sendVersion)));
+        final LiteralCommandNode<ServerCommandSource> oaCommandNode = dispatcher
+                .register(literal("openaudiomc").requires(source -> source.hasPermissionLevel(2))
+                        .then(literal("playlist")
+                                .then(literal("create")
+                                        .then(argument("playlistName", StringArgumentType.string())
+                                                .executes(OpenAudioMcCommand::playlistCreate)))
+                                .then(literal("delete")
+                                        .then(argument("playlistName", StringArgumentType.string())
+                                                .suggests(playlistsSuggestor())
+                                                .executes(OpenAudioMcCommand::playlistDelete)))
+                                .then(literal("list")
+                                        .executes(OpenAudioMcCommand::playlistList))
+                                .then(literal("remove")
+                                        .then(argument("playlistName", StringArgumentType.string())
+                                                .suggests(playlistsSuggestor())
+                                                .then(argument("track_index", IntegerArgumentType.integer())
+                                                        .executes(OpenAudioMcCommand::playlistRemove))))
+                                .then(literal("view")
+                                        .then(argument("playlistName", StringArgumentType.string())
+                                                .suggests(playlistsSuggestor())
+                                                .executes(OpenAudioMcCommand::playlistView)))
+                                .then(literal("add"))
+                                .executes(OpenAudioMcCommand::sendVersion)));
         dispatcher.register(literal("oa").redirect(oaCommandNode));
         dispatcher.register(literal("oam").redirect(oaCommandNode));
         dispatcher.register(literal("openaudio").redirect(oaCommandNode));
@@ -71,8 +85,13 @@ public class OpenAudioMcCommand {
     private static PlaylistService playlistService = (PlaylistService.class)
             .cast(OpenAudioMc.getInstance().getServiceManager().loadService(PlaylistService.class));
 
+    private static PlaylistService getPlaylistService()
+    {
+        return (PlaylistService.class).cast(OpenAudioMc.getInstance().getServiceManager().loadService(PlaylistService.class));
+    }
+
     public static int playlistCreate(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        String name = StringArgumentType.getString(context, "playlist");
+        String name = StringArgumentType.getString(context, "playlist").toLowerCase(Locale.ROOT);
 
         if (playlistService.getPlaylist(name) != null) {
             context.getSource().sendError(Text.literal("A playlist with that name already exists"));
@@ -80,8 +99,7 @@ public class OpenAudioMcCommand {
         }
 
         playlistService.createPlaylist(name, context.getSource().getName());
-        (PlaylistService.class).cast(OpenAudioMc.getInstance().getServiceManager().loadService(PlaylistService.class))
-                .saveAll();
+        getPlaylistService().saveAll();
         context.getSource().sendFeedback(() -> {
             return Text.literal("Created a new playlist with the name " + name);
         }, false);
@@ -89,7 +107,7 @@ public class OpenAudioMcCommand {
     }
 
     private static int playlistDelete(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        String name = StringArgumentType.getString(context, "playlist");
+        String name = StringArgumentType.getString(context, "playlist").toLowerCase(Locale.ROOT);
 
         if (playlistService.getPlaylist(name) == null) {
             context.getSource().sendError(Text.literal("A playlist with that name does not exist"));
@@ -97,11 +115,68 @@ public class OpenAudioMcCommand {
         }
 
         playlistService.deletePlaylist(name);
-        (PlaylistService.class).cast(OpenAudioMc.getInstance().getServiceManager().loadService(PlaylistService.class))
-                .saveAll();
+        getPlaylistService().saveAll();
         context.getSource().sendFeedback(() -> {
             return Text.literal("Deleted the playlist with the name " + name);
         }, false);
         return 1;
+    }
+
+    private static int playlistList(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        Collection<Playlist> playlists = playlistService.getAll();
+
+        if (playlists.isEmpty()) {
+            context.getSource().sendError(Text.literal("There are no playlists"));
+            return 0;
+        }
+
+        context.getSource().sendFeedback(() -> {
+            return Text.literal("There are " + playlists.size() + " playlists");
+        }, false);
+        for (Playlist playlist : playlists) {
+            context.getSource().sendFeedback(() -> {
+                return Text.literal(" - " + playlist.getName() + " by " + playlist.getCreatedBy());
+            }, false);
+        }
+        return 1;
+    }
+
+    private static int playlistRemove(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        Playlist playlist = playlistService.getPlaylist(StringArgumentType.getString(context, "playlist").toLowerCase(Locale.ROOT));
+        Integer index = IntegerArgumentType.getInteger(context, "track_index");
+
+        if (playlist == null) {
+            context.getSource().sendError(Text.literal("A playlist with that name does not exist"));
+            return 0;
+        }
+
+        boolean removed = playlist.removeEntryAt(index);
+        if (!removed) {
+            context.getSource().sendError(Text.literal("There is no track at that index"));
+            return 0;
+        }
+        getPlaylistService().saveAll();
+        context.getSource().sendFeedback(() -> {return Text.literal("Removed track at index " + index + " from playlist " + playlist.getName());}, false);
+        return 1;
+    }
+
+    private static int playlistView(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        Playlist playlist = playlistService.getPlaylist(StringArgumentType.getString(context, "playlist").toLowerCase(Locale.ROOT));
+
+        if (playlist == null) {
+            throw new CommandError("A playlist with that name does not exist");
+        }
+
+        if (playlist.getEntries().isEmpty()) {
+            message(sender, "Playlist " + playlist.getName() + " by " + playlist.getCreatedBy() + " is empty");
+            return;
+        }
+
+        message(sender, "Playlist " + playlist.getName() + " by " + playlist.getCreatedBy() + " has " + playlist.getEntries().size() + " tracks");
+        for (PlaylistEntry orderedEntry : playlist.getOrderedEntries()) {
+            clickable(sender, OaColor.RED + " - " + OaColor.DARK_RED + orderedEntry.getIndex() + OaColor.AQUA + " " + orderedEntry.getMedia(), "openaudio playlist remove " + playlist.getName() + " " + orderedEntry.getIndex());
+        }
+
+        message(sender, "You can click on a track to remove it from the playlist"); 
     }
 }
